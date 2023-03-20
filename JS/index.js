5b5c41774ac1a513ae24be34f85c4621c98fbf79
Moritz Zschunke <moritz_z17@web.de>
2023-03-20 08:40:01 +0100

    added js file

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+const quizcard = document.querySelector('[data-js="quizcard"]');
+const button = document.querySelector('[data-js="button"]');
+
+button.addEventListener("click", () => {
+    quizcard.classList.add("container1-answer");
+}); 
+
+
+
+
