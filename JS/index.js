f525f0da3b6c8251bd16edf158e8a8d40db2ded6
Moritz Zschunke <moritz_z17@web.de>
2023-03-22 20:35:36 +0100

    implemented first inner html to change first container - works :)

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,33 @@
-const quizcard = document.querySelector('[data-js="quizcard"]');
-const button = document.querySelector('[data-js="button"]');
+const quizCard = document.querySelector('[data-js="quizcard"]');
+const answerButton = document.querySelector('[data-js="answerbutton"]');
 
+/*  ---- 1. Versuch vom 20.03.23:
 button.addEventListener("click", () => {
     quizcard.classList.add("container1-answer");
-}); 
+}); */
+
+
+answerButton.addEventListener("click", () => {
+    quizCard.innerHTML ="";
+    quizCard.innerHTML =`
+    <article><p class="questions">What property flips the axes in flexbox?</p></article>
+    <ul class="hash-buttons">
+            <li><button class="tags">#html</button></li>
+            <li><button class="tags">#flexbox</button></li>
+            <li><button class="tags">#css</button></li>
+    </ul>
+    <button class="bookmark-button"><img src="img/bookmark-svgrepo-com.svg" alt="bookmark this" class="quiz-bookmark"></button>
+    `
+    quizCard.style.backgroundColor = "BlanchedAlmond";
+    quizCard.style.border =  "thick solid #0000FF"; 
+});
+
+// quizCard.innerHTML ="";
+
+
+
+
+
 
 
 
