ee752bb4b06cff3930894574777b40d8db0e3eaa
Moritz Zschunke <moritz_z17@web.de>
2023-03-22 21:07:25 +0100

    added the first answer (new <p>) & a new class for it, styling at the bottom of .css

--- JS/index.js
+++ JS/index.js
@@ -6,11 +6,13 @@ button.addEventListener("click", () => {
     quizcard.classList.add("container1-answer");
 }); */
 
+// ----- 2. Versuch, nun über: quizCard.innerHTML ="";
 
 answerButton.addEventListener("click", () => {
     quizCard.innerHTML ="";
     quizCard.innerHTML =`
     <article><p class="questions">What property flips the axes in flexbox?</p></article>
+    <p class="answer1">flex-box</p>
     <ul class="hash-buttons">
             <li><button class="tags">#html</button></li>
             <li><button class="tags">#flexbox</button></li>
@@ -19,10 +21,11 @@ answerButton.addEventListener("click", () => {
     <button class="bookmark-button"><img src="img/bookmark-svgrepo-com.svg" alt="bookmark this" class="quiz-bookmark"></button>
     `
     quizCard.style.backgroundColor = "BlanchedAlmond";
-    quizCard.style.border =  "thick solid #0000FF"; 
+    quizCard.style.border =  "thick inset #A52A2A";
+
 });
 
-// quizCard.innerHTML ="";
+
 
 
 
